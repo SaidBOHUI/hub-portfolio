9f8d7449646ff9035ffcc5768a3dae40116908c6
Saïd Bohui <bohuisaid@hotmail.fr>
2026-02-10 10:51:12 +0100

    test image photobooth

--- src/data/sites.ts
+++ src/data/sites.ts
@@ -53,7 +53,7 @@ export const sites: Site[] = [
     description: "Site Vitrine pour un prestataire de service de photobooth pour évènnements",
     url: "https://photobooth.sbohui.fr",
     github: "https://github.com/SaidBOHUI/photobooth",
-    screenshot: "/screenshots/placeholder-6.svg",
+    screenshot: "/screenshots/photobooth.png",
     tags: ["React", "Typescript", "Tailwind"],
   },
 ];
