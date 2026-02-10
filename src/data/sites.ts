a20a4467050c20ebf80a5bef7876283062d585bf
Saïd Bohui <bohuisaid@hotmail.fr>
2026-02-10 17:07:06 +0100

    change image photobooth

--- src/data/sites.ts
+++ src/data/sites.ts
@@ -53,7 +53,7 @@ export const sites: Site[] = [
     description: "Site Vitrine pour un prestataire de service de photobooth pour évènnements",
     url: "https://photobooth.sbohui.fr",
     github: "https://github.com/SaidBOHUI/photobooth",
-    screenshot: "/screenshots/photobooth.png",
+    screenshot: "/screenshots/photobooth3.png",
     tags: ["React", "Typescript", "Tailwind"],
   },
 ];
